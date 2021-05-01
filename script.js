4f7c06fbfd9b4601ac6123a3113e5055aa45074d
Rob Pupo <pupo.robert@gmail.com>
2021-05-01 11:52:43 -0400

    local storage logic, cleaning some superfluous code

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-var saveEl = $('.saveBtn');
+var save = $('.saveBtn');
 var textEl = $('.text');
 var hours = [$('#text09'), $('#text10'), $('#text11'), $('#text12'), $('#text01'), $('#text02'), $('#text03'), $('#text04'),$('#text05')]
 var currentTime = parseInt(moment().format('HH'));
 
-$("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));   
 
 var colors = function ()  {
     for (i=0; i < hours.length; i++) {
@@ -18,6 +17,38 @@ var colors = function ()  {
     }
 }
 
+function saveButton() {
+    var existingEntries = [];
+    for (i=0; i < hours.length; i++) {
+        var entry = {
+            "name": i+9,
+            "entry": hours[i][0].value
+        };
+        existingEntries.push(entry);
+    }   
+    localStorage.setItem("allEntries", JSON.stringify(existingEntries));
+}
+
+function addListeners() {
+    for(i=0; i<save.length;i++) {
+        save[i].addEventListener("click", saveButton)
+    }
+}
+
+function loadEntries() {
+    var existingEntries = JSON.parse(localStorage.getItem("allEntries"));
+    if(existingEntries == null) {existingEntries = [];}
+    for (i=0; i < hours.length; i++) {
+        hours[i][0].textContent = existingEntries[i].entry;
+        console.log(existingEntries[i].entry)
+    }   
+}
 
 
+
+
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));   
+addListeners();
 colors();
+loadEntries();
+
